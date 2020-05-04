47a771d5403826c09615ad6e36d4073cf56e1d77
yhmane <hwang9yh@gmail.com>
2020-05-04 23:07:21 +0900

    input advanced example
    객체를 update시 spread 문법 사용 (불변성을 지켜주기 위해)

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,22 +1,45 @@
 import React, { useState } from 'react';
 
 function InputSample() {
-    const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: '',
+    });
+
+    const { name, nickname } = inputs;
 
     const onChange = (e) => {
-        setText(e.target.value);
+        const { name, value } = e.target;
+        setInputs({
+            ...inputs,      // spread 문법
+            [name]: value,  // [name] name 또는 nickname이 온다 괄호를 줄것
+        }); // 객체를 설정
     };
 
     const onReset = () => {
-        setText('');
+        setInputs({
+            name: '',
+            nickname: '',
+        });
     };
     return (
         <div>
-            <input onChange={onChange} value={text} />
+            <input 
+                name="name" 
+                placeholder="이름" 
+                onChange={onChange} 
+                value={name}
+            />
+            <input 
+                name="nickname" 
+                placeholder="닉네임" 
+                onChange={onChange}
+                value={nickname}
+            />
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값: </b>
-                {text}
+                {name} ({nickname})
             </div>
         </div>
     )
